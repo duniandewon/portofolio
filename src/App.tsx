6ed3fe4aa51311f203844771246396016f7ecc60
Mohammad Baragbah <duniandewon@gmail.com>
2023-01-11 22:26:11 +0700

    Change font

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import SideBar from "./components/SideBar";
 
 import Wrapper from "./components/Wrapper";
 
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+
 function App() {
   return (
     <Wrapper>
--- src/components/MainContent/Intro.tsx
+++ src/components/MainContent/Intro.tsx
@@ -4,15 +4,13 @@ import Typography from "@mui/material/Typography";
 
 const Intro = () => {
   return (
-    <Box 
-    sx={{ pt: { lg: 27, xl: 38 } }}
-    >
+    <Box sx={{ pt: { lg: 15, xl: 23 } }}>
       <Typography variant="h6">Hi there,</Typography>
-      <Typography variant="h2" fontWeight={900}>
+      <Typography variant="h2" fontSize={{ lg: 80, xl: 100 }} fontWeight={900}>
         I am Abolfaz
       </Typography>
-      <Typography variant="h6">I create websites and apps</Typography>
-      <Button variant="contained" sx={{ mt: 2 }}>
+      <Typography variant="h6" fontSize={{ lg: 34, xl: 40 }}>I create websites and apps</Typography>
+      <Button variant="contained" sx={{ mt: 3 }} size="large">
         Contact Me
       </Button>
     </Box>
