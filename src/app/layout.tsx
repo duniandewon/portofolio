039f86559878c453539bbc753b01695db71610b0
Mohammad Baragbah <duniandewon@gmail.com>
2023-05-27 13:37:36 +0700

    Create nextjs project and configs

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+
+import StyledComponentsRegistry from "@/lib/registry";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "My Portofolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
