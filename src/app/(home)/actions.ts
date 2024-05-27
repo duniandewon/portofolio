30eca09e1d59fdf1c715f168d4d2e580351df2b5
aboli <duniandewon@gmail.com>
2024-05-27 16:48:54 +0700

    refactor: convert prisma create return type to Post

--- src/app/(home)/actions.ts
+++ src/app/(home)/actions.ts
@@ -1,19 +1,19 @@
 "use server"
 
 import {redirect} from "next/navigation";
-import {type Post} from "@prisma/client";
+import {Post} from "@prisma/client";
 
 import prisma from "@/lib/prisma-client";
 
 export async function createNewPost() {
-    let post: Post | null = null
+    let post: Post | null = null;
 
     try {
-        post = await prisma.post.create({
+        post = (await prisma.post.create({
             data: {
                 title: "Untitled Project"
             }
-        })
+        })) as Post
     } catch (e) {
         console.log("Something went wrong:", e)
     }
